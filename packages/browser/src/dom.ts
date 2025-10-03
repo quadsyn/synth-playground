b0812a70ec3f633151cf8ae0e747df3635734233
Pixel <calebrtomlinson@gmail.com>
2025-10-03 13:39:17 -0700

    Comment dom.ts and add a fast string-to-HTML parse function

--- packages/browser/src/dom.ts
+++ packages/browser/src/dom.ts
@@ -1,3 +1,10 @@
+/**
+ * Creates an HTML element with attributes and children, e.g.  
+ * H('div', { class: 'one two' })
+ * 
+ * Use document.createTextNode() for unknown text, or T() for localized text  
+ * Use S() for SVG elements which are different than HTML.
+ */
 export function H<K extends keyof HTMLElementTagNameMap>(
     elementType: K,
     attributes: { [key: string]: string },
@@ -16,6 +23,13 @@ export function H<K extends keyof HTMLElementTagNameMap>(
     return element;
 }
 
+/**
+ * Creates an SVG element with attributes and children, e.g.
+ * S('svg', {}, S('path', { d: 'values here' }))
+ * 
+ * Use H() for HTML elements.
+ * Use parse() to convert an HTML string to elements without type safety. This is practical for static SVGs.
+ */
 export function S<K extends keyof SVGElementTagNameMap>(
     elementType: K,
     attributes: { [key: string]: string },
@@ -34,4 +48,17 @@ export function S<K extends keyof SVGElementTagNameMap>(
         );
     }
     return element;
+}
+
+/**
+ * Parses an HTML string to html element(s) and returns them. This can inject script nodes; do not expose to user
+ * input. Chunks of static HTML are ideal for SVGs; prefer H() and S() to generate HTML or SVG elements normally.
+ */
+export function parse(htmlStr: string): ChildNode | NodeListOf<ChildNode> | null {
+    const template = document.createElement('template');
+    template.innerHTML = htmlStr.trim();
+
+    return template.content.childNodes.length === 0 ? null
+        : template.content.childNodes.length === 1 ? template.content.firstChild
+        : template.content.childNodes;
 }
